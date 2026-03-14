c060d1275002cf365be135cc15a969ed853b08b1
MiqueasCast <castmicah@gmail.com>
2026-03-13 22:27:29 -0600

    Desarrollo del programa

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+
+void Menu()
+{
+    Console.Write("Menú: \n1. Calcular el área de un círuclo\n2. Calcular el área de un triángulo\n3. Calcular el factorial de un número\n" +
+                  "4. Determinar si un número es par o impar\n5. Salir");
+}
+
+double AreaCirculo(double radio)
+{
+    double area = Math.PI * Math.Pow(radio, 2);
+    return area;
+}
+
+
+
+
+
+int opcion = 0;
+do
+{
+    Menu();
+    Console.WriteLine();
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            Console.Write("Ingrese radio del circulo: ");
+            Console.WriteLine("Área:"+AreaCirculo(double.Parse(Console.ReadLine())));
+            break;
+        case 2:
+
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            break;
+    }
+
+
+
+}while( opcion != 5);
